2746c8566c1141a9a0666b9734eb3f650e3b0dbc
Sean <wxy.6056@gmail.com>
2022-11-01 16:40:39 -0500

    remove violations

--- src/main/java/edu/utsa/cs/App.java
+++ src/main/java/edu/utsa/cs/App.java
@@ -18,10 +18,11 @@ public class App
 
     public static void main( String[] args )
     {
-		try{
-			System.out.println( "Hello World!");
-		}catch(RuntimeException e){
-			e.printStackTrace();
-		}
+//		try{
+//			System.out.println( "Hello World!");
+//		}catch(RuntimeException e){
+//			e.printStackTrace();
+//		}
+		
     }
 }
