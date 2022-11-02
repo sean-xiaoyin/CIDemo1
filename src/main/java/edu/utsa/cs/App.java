b8e8817108458ed4113a2fb665cffbfbd3ff04a1
Sean <wxy.6056@gmail.com>
2022-11-01 20:11:13 -0500

    add another violation

--- src/main/java/edu/utsa/cs/App.java
+++ src/main/java/edu/utsa/cs/App.java
@@ -18,11 +18,11 @@ public class App
 
     public static void main( String[] args )
     {
-//		try{
+		try{
 			System.out.println( "Hello World!");
-//		}catch(RuntimeException e){
-//			e.printStackTrace();
-//		}
+		}catch(RuntimeException e){
+			e.printStackTrace();
+		}
 		
     }
 }
